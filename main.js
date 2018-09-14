3692824ce6df5c5d4bf284da3a8894f80da78155
Małgorzata Osinska <m.osinska2017@gmail.com>
2018-09-14 12:11:47 +0200

    liczby true/false

---
+++ main.js
@@ -0,0 +1,13 @@
+/*var liczba1= 2;
+var liczba2= 18;
+*/
+function multiply (liczba1,liczba2){
+    var result
+    if (liczba1==20||liczba2==20||liczba1+liczba2<=20){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
+    return result;
+ }
+multiply();
